cba1bf6a5188bac562ba9217038078b9635d73fd
Karamveer Singh Oberoi <71765505+karamveer2648@users.noreply.github.com>
2025-04-23 22:44:44 +0530

    czc
    czczc

--- main.js
+++ main.js
@@ -803,7 +803,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 { name: "MySQL", color: "cyan" },
                 { name: "JavaScript", color: "yellow" }
             ],
-            link: "#",
+            link: "BikeMahiti.html",
             linkText: "View Project",
             headerColor: { from: "red-500", to: "pink-600" },
             icon: '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path>'
